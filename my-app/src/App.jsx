e21d4745aebdab880c160cbd7bca52c48fa0bd6e
Saiprem <katukamsaiprem@gmail.com>
2025-04-08 12:28:26 +0530

    ToDoCompleted

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,20 +2,42 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { v4 as uuid } from "uuid";
 
 function App() {
-  
+
   const [todo,setTodo]=useState()
+  const [todoList,setTodoList]=useState([])//add to arr for store todo items
 
   const onToDoInput=(e)=>{
     setTodo(e.target.value);
   };
   const onAddtodo=(e)=>{
     
+    if(todo.trim()===""){
+      alert("todo cannot be empty")
+      return;
+    }
+
+
+    setTodoList([...todoList,{id: uuid(),todo:todo,isCompleted:false}])//on click add ,set todo list with value in todo state,when ever we want to add value to arr or obj,first spread it,set the value in the form of obj,because we need key to identify
+    //to get the id ,install package
+    setTodo("")//clear input after adding
   };
 
-  return (
+  const onClickDelete=(id)=>{
+    const updatedList=todoList.filter(todo=>todo.id!==id);
+    setTodoList(updatedList)
+
+  };
+  const onClickCheckboxChange=(id)=>{
+    const updatedList=todoList.map(todo=>todo.id===id?{...todo,  isCompleted: !todo.isCompleted}:todo)
+    setTodoList(updatedList)
+    console.log(updatedList)
+
+  }
+
+  return (  
     <>
       <h1>My ToDoList</h1>
       <div>
@@ -23,20 +45,26 @@ function App() {
         <button onClick={onAddtodo}>ADD </button>
       </div>
       <div>
-        <div>
-          <label>
-            <input type="checkbox" />
-            <span>awdawd</span>
-            <button>delete</button>
-          </label>
-        </div>
+        {
+           todoList?.length>0 && todoList.map(todo =>(
+            <div key={todo.id} >
+              <label>
+                <input onClick={()=>onClickCheckboxChange(todo.id)} type="checkbox"/>
+                <span className={todo.isCompleted?'strike-through':''}>{todo.todo}</span>
+                </label>
+                <button onClick={()=>onClickDelete(todo.id)} >Delete</button>
+
+            </div>
+          ))
+        }
       </div>
-      
+
     </>
   );
 }
 
 export default App;
+
 
 //todo
 // features-adding data,checkbox for each data,able to delete
